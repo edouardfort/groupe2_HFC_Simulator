b4b23a1fbc58339dae1b87760b278c6393f0d218
Jordaneuh <jordan.tcha@mmibordeaux.com>
2025-03-18 16:50:53 +0100

    Test jojo map

--- Assets/Scripts/Mec cuisson/TimerFriteuse.cs
+++ Assets/Scripts/Mec cuisson/TimerFriteuse.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class TimerFriteuse : MonoBehaviour
 {
     public TextMeshPro timerText; // Référence au texte 3D
+    public TextMeshPro messafterfinish; // Texte 3D après le décompte
     public float tempsDeCuisson; //= 20f; // Temps en secondes, je l'ai mit en commentaire psk ça changeait rien du tout
     private float tempsRestant;
     private bool enCuisson = false;
@@ -13,6 +14,7 @@ public class TimerFriteuse : MonoBehaviour
     void Start()
     {
         timerText.gameObject.SetActive(false); // Cache le timer au début
+        messafterfinish.gameObject.SetActive(false); // Cache le texte au début
     }
 
     void Update()
@@ -29,6 +31,7 @@ public class TimerFriteuse : MonoBehaviour
                 enCuisson = false;
                 timerText.gameObject.SetActive(false); // Cache le timer à la fin
                 Debug.Log("Les frites sont prêtes !"); //Verif, changer cette ligne avec une phrase pour dire que c'est cuit ==> prochain mvp faire un temps pour voir si ils vont cramés ou pas.
+                messafterfinish.gameObject.SetActive(true); // Montre le message prêt
             }
 
         }
@@ -47,6 +50,7 @@ public class TimerFriteuse : MonoBehaviour
             enCuisson = true;
             tempsRestant = tempsDeCuisson;
             timerText.gameObject.SetActive(true); // Affiche le timer
+            messafterfinish.gameObject.SetActive(false); // Cache le mess pendant le cuisson
         }
     }
 }
