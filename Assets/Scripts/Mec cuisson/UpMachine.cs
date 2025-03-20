85ebd115539d23582a4c86007e606b8b985225d2
Jordaneuh <jordan.tcha@mmibordeaux.com>
2025-03-20 18:04:31 +0100

    Diff prix d'amélioration

--- Assets/Scripts/Mec cuisson/UpMachine.cs
+++ Assets/Scripts/Mec cuisson/UpMachine.cs
@@ -8,11 +8,19 @@ public class UpMachine : MonoBehaviour
 
     void OnMouseDown() // Détection du clic sur l'objet
     {
-        if (Argent.instance.argent >= 5 && timerFriteuse.nivmachine < 3)  // On récup la variable argent et on vérifie si elle a la tune nécessaire
+        if (Argent.instance.argent >= 5 && timerFriteuse.nivmachine == 0)  // On récup la variable argent et on vérifie si elle a la tune nécessaire
         {
-            Argent.instance.gagnerArgent(-5); // Retire 20 d'argent
+            Argent.instance.gagnerArgent(-5); // Retire 5 d'argent
             timerFriteuse.AmeliorerMachine();  // Améliore la machine
-        } else
+        } else if (Argent.instance.argent >= 10 && timerFriteuse.nivmachine == 1)
+        {
+            Argent.instance.gagnerArgent(-10); // Retire 5 d'argent
+            timerFriteuse.AmeliorerMachine();
+        } else if(Argent.instance.argent >= 15 && timerFriteuse.nivmachine == 2)
+        {
+            Argent.instance.gagnerArgent(-15); // Retire 5 d'argent
+            timerFriteuse.AmeliorerMachine();
+        } else 
         {
             Debug.Log("Pas assez d'argent ou niveau max atteint !");
         }
