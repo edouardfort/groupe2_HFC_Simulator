7da654d5e2f3952f04c00daab05071f28cf7a182
EricTrky <erictranky@gmail.com>
2025-03-20 21:45:58 +0100

    Bouton amélioration rouge vert gris

--- Assets/Scripts/Mec cuisson/UpMachine.cs
+++ Assets/Scripts/Mec cuisson/UpMachine.cs
@@ -4,7 +4,30 @@ using UnityEngine;
 
 public class UpMachine : MonoBehaviour
 {
-        public TimerFriteuse timerFriteuse;  // Référence au script TimerFriteuse
+    public Material redMaterial;   // Matériau rouge
+    public Material grayMaterial;  // Matériau gris
+    public Material greenMaterial; // Matériau vert
+
+    public TimerFriteuse timerFriteuse;  // Référence au script TimerFriteuse
+
+    void Update()
+    {
+        if (timerFriteuse.nivmachine==3){
+            GetComponent<Renderer>().material = grayMaterial;
+
+        }
+        else if (Argent.instance.argent >= 5 * (timerFriteuse.nivmachine + 1))
+        {
+            GetComponent<Renderer>().material = greenMaterial;
+
+        }
+        else if (Argent.instance.argent < 5 * (timerFriteuse.nivmachine + 1))
+        {
+            GetComponent<Renderer>().material = redMaterial;
+
+        }
+        
+    }
 
     void OnMouseDown() // Détection du clic sur l'objet
     {
@@ -12,15 +35,18 @@ public class UpMachine : MonoBehaviour
         {
             Argent.instance.gagnerArgent(-5); // Retire 5 d'argent
             timerFriteuse.AmeliorerMachine();  // Améliore la machine
-        } else if (Argent.instance.argent >= 10 && timerFriteuse.nivmachine == 1)
+        }
+        else if (Argent.instance.argent >= 10 && timerFriteuse.nivmachine == 1)
         {
             Argent.instance.gagnerArgent(-10); // Retire 5 d'argent
             timerFriteuse.AmeliorerMachine();
-        } else if(Argent.instance.argent >= 15 && timerFriteuse.nivmachine == 2)
+        }
+        else if (Argent.instance.argent >= 15 && timerFriteuse.nivmachine == 2)
         {
             Argent.instance.gagnerArgent(-15); // Retire 5 d'argent
             timerFriteuse.AmeliorerMachine();
-        } else 
+        }
+        else
         {
             Debug.Log("Pas assez d'argent ou niveau max atteint !");
         }
